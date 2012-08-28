e2e4ca95eb3d072082044698baf71414e8723aae
Steven Haryanto (on PC) <stevenharyanto@gmail.com>
2012-08-28 09:30:31 +0700

    Adjust setup_dir's metadata

--- lib/Setup/File/Dir.pm
+++ lib/Setup/File/Dir.pm
@@ -5,7 +5,7 @@ use strict;
 use warnings;
 use Log::Any '$log';
 
-use Setup::File;
+#use Setup::File;
 
 require Exporter;
 our @ISA       = qw(Exporter);
@@ -16,36 +16,36 @@ our @EXPORT_OK = qw(setup_dir);
 our %SPEC;
 
 $SPEC{setup_dir} = {
-    summary  => "Setup directory (existence, mode, permission)",
+    v           => 1.1,
+    summary     => "Setup directory (existence, mode, permission)",
     description => <<'_',
 
 On do, will create directory (if it doesn't already exist) and fix its
 mode/permission.
 
 On undo, will restore old mode/permission (and delete directory if it is empty
-and was created by this function).
-
-If given, -undo_hint should contain {tmp_dir=>...} to specify temporary
-directory to save replaced file/dir. Temporary directory defaults to ~/.setup,
-it will be created if not exists.
+and was created by this function). If directory was created by this function but
+is not empty, will return status 331 to ask for confirmation (`-confirm`). If
+confirmation is set to true, will delete non-empty directory.
 
 Will *not* create intermediate directories like "mkdir -p". Create intermediate
 directories using several setup_dir() invocation.
 
 _
     args     => {
-        path => ['str*' => {
-            summary => 'Path to dir',
+        path => {
+            schema      => ['str*' => {match => qr!^/!}],
+            pos         => 0,
+            summary     => 'Path to dir',
             description => <<'_',
 
 Dir path needs to be absolute so it's normalized.
 
 _
-            arg_pos => 1,
-            match   => qr!^/!,
-        }],
-        should_exist => ['bool' => {
-            summary => 'Whether dir should exist',
+        },
+        should_exist => {
+            schema      => ['bool' => {}],
+            summary     => 'Whether dir should exist',
             description => <<'_',
 
 If undef, dir need not exist. If set to 0, dir must not exist and will be
@@ -53,18 +53,22 @@ deleted if it does. If set to 1, dir must exist and will be created if it
 doesn't.
 
 _
-        }],
-        mode => ['str' => {
+        },
+        mode => {
+            schema  => ['str' => {}],
             summary => 'Expected permission mode',
-        }],
-        owner => ['str' => {
+        },
+        owner => {
+            schema  => ['str' => {}],
             summary => 'Expected owner',
-        }],
-        group => ['str' => {
+        },
+        group => {
+            schema   => ['str' => {}],
             summary => 'Expected group',
-        }],
-        allow_symlink => ['bool*' => {
-            summary => 'Whether symlink is allowed',
+        },
+        allow_symlink => {
+            schema      => ['bool*' => {default=>1}],
+            summary     => 'Whether symlink is allowed',
             description => <<'_',
 
 If existing dir is a symlink then if allow_symlink is false then it is an
@@ -74,22 +78,24 @@ true).
 Note: if you want to setup symlink instead, use Setup::Symlink.
 
 _
-            default => 1,
-        }],
-        replace_symlink => ['bool*' => {
+        },
+        replace_symlink => {
+            schema  => ['bool*' => {default=>1}],
             summary => "Replace existing symlink if it needs to be replaced",
-            default => 1,
-        }],
-        replace_file => ['bool*' => {
+        },
+        replace_file => {
+            schema  => ['bool*' => {default=>1}],
             summary => "Replace existing file if it needs to be replaced",
-            default => 1,
-        }],
-        replace_dir => ['bool*' => {
+        },
+        replace_dir => {
+            schema  => ['bool*' => {default=>1}],
             summary => "Replace existing dir if it needs to be replaced",
-            default => 1,
-        }],
+        },
     },
-    features => {undo=>1, dry_run=>1},
+    features => {
+        tx         => {v=>2},
+        idempotent => 1,
+    },
 };
 sub setup_dir  {
     my %args = @_;
@@ -98,42 +104,11 @@ sub setup_dir  {
 
 1;
 # ABSTRACT: Setup directory (existence, mode, permission)
-__END__
-
-=head1 SYNOPSIS
-
- use Setup::File::Dir 'setup_dir';
-
- # simple usage (doesn't save undo data)
- $res = setup_dir path => '/etc/myapp',
-                  should_exist => 1,
-                  owner => 'root', group => 0, mode => 0755;
- die unless $res->[0] == 200;
-
- # perform setup and save undo data (undo data should be serializable)
- $res = setup_dir ..., -undo_action => 'do';
- die unless $res->[0] == 200;
- my $undo_data = $res->[3]{undo_data};
-
- # perform undo
- $res = setup_dir ..., -undo_action => "undo", -undo_data=>$undo_data;
- die unless $res->[0] == 200;
-
- # state that dir must not exist
- setup_dir path => '/foo/bar', should_exist => 0;
-
-
-=head1 DESCRIPTION
-
-This module uses L<Log::Any> logging framework.
-
-This module has L<Rinci> metadata.
-
 
 =head1 SEE ALSO
 
-L<Setup::File>
-
 L<Setup>
+
+L<Setup::File>
 
 =cut
